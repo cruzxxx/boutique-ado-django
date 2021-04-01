f22cb975b3aa67e9fee4e1bfeaf21084738be6ee
stefcruz <crzstefanie@gmail.com>
2021-04-01 19:17:19 +0000

    add checkout views and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -2,13 +2,13 @@ from django import forms
 from .models import Order
 
 
-class OrderForm(forms.Modelform):
+class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
-                'street_address1', 'street_address2',
-                'town_or_city', 'postcode', 'country',
-                'county',)
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
 
     # overwriting init method
     def __init__(self, *args, **kwargs):
